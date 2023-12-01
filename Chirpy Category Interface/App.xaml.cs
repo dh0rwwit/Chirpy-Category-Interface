50735ee0fb75e19cb8ca08a7765310c9d055f11a
dh0rwwit <dh0rwwit@gmail.com>
2023-12-01 12:33:26 +0900

    c

---
+++ Chirpy Category Interface/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Chirpy_Category_Interface
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
